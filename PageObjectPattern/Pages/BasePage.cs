3fd6fc19eeda080277c0cb6e66ba4183c8977aa0
webberbond <sergey.zrch@gmail.com>
2023-02-09 20:25:30 +0500

    Added project files

---
+++ PageObjectPattern/Pages/BasePage.cs
@@ -0,0 +1,44 @@
+﻿namespace WebDriverBasics.Pages;
+
+public abstract class BasePage
+{
+    protected IWebDriver WebDriver { get; }
+
+    protected BasePage(IWebDriver webDriver)
+    {
+        WebDriver = webDriver;
+    }
+
+
+    private IWebElement UniqueWebElement => WebDriver.FindElement(UniqueWebLocator);
+
+    protected abstract By UniqueWebLocator { get; }
+
+    private readonly string _baseUrl = Configurator.BaseUrl;
+
+    protected abstract string UrlPath { get; }
+
+    public void OpenPage()
+    {
+        var uri = new Uri(_baseUrl.TrimEnd('/') + UrlPath, UriKind.Absolute);
+        WebDriver.Navigate().GoToUrl(uri);
+    }
+
+    public bool IsPageOpened
+    {
+        get
+        {
+            bool isOpened;
+            try
+            {
+                isOpened = UniqueWebElement.Displayed;
+            }
+            catch (Exception e)
+            {
+                isOpened = false;
+            }
+
+            return isOpened;
+        }
+    }
+}
---
+++ PageObjectPattern/Pages/HomePage.cs
@@ -0,0 +1,20 @@
+﻿namespace WebDriverBasics.Pages;
+
+public class HomePage : BasePage
+{
+    public HomePage(IWebDriver webDriver) : base(webDriver)
+    {
+        PageFactory.InitElements(webDriver, this);
+    }
+
+    protected override By UniqueWebLocator => By.XPath("//div[@class='logo__icon']");
+    protected override string UrlPath => string.Empty;
+
+    [FindsBy(How = How.XPath, Using = "//a[contains(.,'HERE')]")]
+    private IWebElement NextPageButton;
+
+    public void GoToNextPage()
+    {
+        NextPageButton.Click();
+    }
+}
