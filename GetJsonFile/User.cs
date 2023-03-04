63ab6589c4a91eb1ad504787c2f5a3aba61554e4
webberbond <sergey.zrch@gmail.com>
2023-03-05 00:31:36 +0500

    Added postman collection & Made project for writing json

---
+++ GetJsonFile/User.cs
@@ -0,0 +1,18 @@
+﻿namespace GetJsonFile;
+
+public class User
+{
+    public string Name { get; set; }
+
+    public object LastName { get; set; }
+
+    public dynamic Age { get; set; }
+
+    public ICollection<int> RecordAmount { get; set; }
+
+    public bool IsMale { get; set; }
+
+    public double WalletAmount { get; set; }
+
+    public string[] FavouriteSongs { get; set; }
+}
---
+++ GetJsonFile/Utils/JsonFileUtil.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace GetJsonFile.Utils;
+
+public static class JsonFileUtil
+{
+    private static readonly JsonSerializerOptions Options =
+        new() {DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull};
+
+    public static void SimpleWrite(object obj, string fileName)
+    {
+        var jsonString = JsonSerializer.Serialize(obj, Options);
+        File.WriteAllText(fileName, jsonString);
+    }
+}
