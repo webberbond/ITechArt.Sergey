cf4a58f949976b0b4d13e46a5c053a078ad425db
webberbond <sergey.zrch@gmail.com>
2023-03-13 19:46:58 +0500

    Edited project

--- GetJsonFile/Program.cs
+++ GetJsonFile/Program.cs
@@ -1,4 +1,4 @@
-﻿using GetJsonFile.Utils;
+﻿using Newtonsoft.Json;
 
 namespace GetJsonFile;
 
@@ -6,28 +6,26 @@ internal static class Program
 {
     public static void Main()
     {
-        var user = new User
-        {
-            Name = "Arslan",
-            LastName = "Berkut",
-            Age = 21,
-            RecordAmount = new List<int> {100, 350, 680, 1000},
-            IsMale = true,
-            WalletAmount = 321.128,
-            FavouriteSongs = new[]
+        User[] users = {
+            new()
             {
-                "NF - Hope",
-                "Drake and 21 Savage - Spin Bout U",
-                "SZA - Low",
-                "Finesse2Tymes - Back End"
+                Name = "Sergey",
+                LastName = "Zarochentsev",
+                Age = "20",
+                RecordAmount = new List<int> {1, 3, 7, 9},
+                IsMale = true,
+                WalletAmount = 100.15,
+                FavouriteSongs = new[]
+                {
+                    "Buddy Holly - Weezer",
+                    "Josie - Blink-182",
+                    "Helena - My Chemical Romance"
+                }
             }
         };
-
-
-        var filepath = "../../../JsonFile";
-        if (!Directory.Exists(filepath)) Directory.CreateDirectory(filepath);
-
-        var filename = "../../../JsonFile/User.json";
-        JsonFileUtil.SimpleWrite(user, filename);
+        
+        string json =  JsonConvert.SerializeObject(users, Formatting.Indented);
+        
+        File.WriteAllText(@"..\..\..\Data\users.json", json);
     }
 }
